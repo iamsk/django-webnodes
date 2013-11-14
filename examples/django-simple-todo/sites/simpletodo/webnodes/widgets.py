839c8cee376707cad37cb069f901eaf4527d7b5b
iamsk <iamsk.info@gmail.com>
2013-11-14 16:31:03 +0800

    add example: django-simple-todo

---
+++ examples/django-simple-todo/sites/simpletodo/webnodes/widgets.py
@@ -0,0 +1,22 @@
+from django_webnodes import WebNode
+
+from simpletodo.models import Todo
+
+
+class CPT_List(WebNode):
+    template = 'components/list.html'
+
+    def get_context(self, a, b, x=None, y=None):
+        todos = Todo.objects.all().order_by('finished', '-id')
+        return {'todos': todos}
+
+
+class CPT_ITEM(WebNode):
+    template = 'components/item.html'
+
+    def get_context(self, todo):
+        return {'todo': todo}
+
+
+class CPT_Add(WebNode):
+    template = 'components/add.html'
